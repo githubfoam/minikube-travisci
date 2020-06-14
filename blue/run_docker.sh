46a16d2a4d04e46b3bfc11ad02c17b2cb22f669f
githubfoam <lequestionis@gmail.com>
2020-06-14 15:13:19 +0300

    How's it look?

--- blue/run_docker.sh
+++ blue/run_docker.sh
@@ -6,10 +6,10 @@
 # Build image and add a descriptive tag
 docker build --tag=testblueimage .
 
-# Step 2: 
+# Step 2:
 # List docker images
 docker image ls
 
-# Step 3: 
+# Step 3:
 # Run flask app
-docker run -p 8000:80 testblueimage
+docker run -p 8000:80 testblueimage &
