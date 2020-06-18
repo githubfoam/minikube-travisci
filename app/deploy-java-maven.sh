e3d4848eddc0bc82472dfd279ce50645d6ef3206
githubfoam <lequestionis@gmail.com>
2020-06-19 01:01:39 +0300

    What are you talking about?

--- app/deploy-java-maven.sh
+++ app/deploy-java-maven.sh
@@ -3,6 +3,8 @@ set -eox pipefail #safety for script
 
 # https://github.com/aws-samples/kubernetes-for-java-developers
 echo "=============================Dashboard============================================================="
+apt-get -qq update && sudo apt-get install -qqy maven && mvn -version
+java -version
 git clone https://github.com/aws-samples/kubernetes-for-java-developers.git
 cd app && mvn spring-boot:run
 echo $(curl http://localhost:8080/hello)
