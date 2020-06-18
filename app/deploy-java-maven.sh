edc95cf0a54df0a354e939bb192572122b495d09
githubfoam <lequestionis@gmail.com>
2020-06-19 01:23:38 +0300

    You blosom in the sun and me,

--- app/deploy-java-maven.sh
+++ app/deploy-java-maven.sh
@@ -5,11 +5,10 @@ set -eox pipefail #safety for script
 #a simple Java application built using Spring Boot. The application publishes a REST endpoint that can be invoked at http://{host}:{port}/hello.
 echo "=============================Java application built using Spring Boot============================================================="
 apt-get -qq update && sudo apt-get install -qqy maven && mvn -version
-#https://stackoverflow.com/questions/30855864/maven-no-plugin-found-for-prefix-spring-boot-in-the-current-project-and-in-th/33602837
 mvn org.springframework.boot:spring-boot-maven-plugin:run
 java -version
 git clone https://github.com/aws-samples/kubernetes-for-java-developers.git
-cd app && mvn spring-boot:run
+cd kubernetes-for-java-developers/app/ && mvn spring-boot:run
 echo $(curl http://localhost:8080/hello)
 echo $(curl http://127.0.0.1:8080/hello)
 echo "=============================Java application built using Spring Boot============================================================="
