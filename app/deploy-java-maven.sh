402d99e97123b2d37f571aa0e133d30c1a53661c
githubfoam <lequestionis@gmail.com>
2020-06-20 19:51:34 +0300

    ста́рая пря́ха

--- app/deploy-java-maven.sh
+++ app/deploy-java-maven.sh
@@ -11,6 +11,9 @@ cd kubernetes-for-java-developers/app/
 ls -lai
 mvn org.springframework.boot:spring-boot-maven-plugin:run
 mvn spring-boot:run
+echo "127.0.0.1      localhost loopback" |sudo tee -a /etc/hosts
 echo $(curl http://localhost:8080/hello)
 echo $(curl http://127.0.0.1:8080/hello)
+curl http://localhost:8080/hello
+curl http://127.0.0.1:8080/hello
 echo "=============================Java application built using Spring Boot============================================================="
