aa658cac13a1cba22d746d10560d8c828e36b1a9
githubfoam <lequestionis@gmail.com>
2020-06-26 16:22:11 +0300

    люб`ых

--- app/deploy-minikube_latest.sh
+++ app/deploy-minikube_latest.sh
@@ -13,10 +13,15 @@ mv ./kubectl /usr/local/bin/kubectl
 # Install Minikube
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.2.0/minikube-linux-amd64
 chmod +x minikube
-cp minikube /usr/local/bin/
-rm minikube
+cp minikube /usr/local/bin/ && rm minikube
 #starts Minikube with 6 CPUs, 12288 memory, 120G disk size
-minikube start --vm-driver=none --cpus 6 --memory 12288 --disk-size=120g --extra-config=apiserver.authorization-mode=RBAC --extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf --extra-config kubeadm.ignore-preflight-errors=SystemVerification
+minikube start --vm-driver=none \
+                --cpus 6 \
+                --memory 12288 \
+                --disk-size=120g \
+                --extra-config=apiserver.authorization-mode=RBAC \
+                --extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf \
+                --extra-config kubeadm.ignore-preflight-errors=SystemVerification
 
 
 #
