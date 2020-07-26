a53307e42d6d7a31ea3585438d9ee6e6d790de4b
githubfoam <lequestionis@gmail.com>
2020-07-26 19:51:31 +0300

    почти́ но́вый

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,7 +6,7 @@ set -o xtrace
 # set -eox pipefail #safety for script
 
 echo "============================provisioning started =============================================================="
-export VAGRANT_USER_HOME=/home/vagrant
+# export VAGRANT_USER_HOME=/home/vagrant
 # export MINIKUBE_WANTUPDATENOTIFICATION=false
 # export MINIKUBE_WANTREPORTERRORPROMPT=false
 # export MINIKUBE_HOME=$VAGRANT_USER_HOME
@@ -23,10 +23,10 @@ export VAGRANT_USER_HOME=/home/vagrant
 # chmod +x *.sh
 # popd
 
-for a in `find home -name "*" -type f` ; do
-  rm -f $VAGRANT_USER_HOME/`basename $a`
-  ln -rs $a /home/vagrant
-done
+# for a in `find home -name "*" -type f` ; do
+#   rm -f $VAGRANT_USER_HOME/`basename $a`
+#   ln -rs $a /home/vagrant
+# done
 echo "export PATH=$PATH:/vagrant/vagrant/bin" >> $VAGRANT_USER_HOME/.bashrc
 
 echo "============================provisioning finished =============================================================="
