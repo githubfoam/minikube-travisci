32add09c8f318f60a2d9cf90f02e88f72b3a2a6c
githubfoam <lequestionis@gmail.com>
2020-07-30 17:45:22 +0300

    locust run

--- app/deploy-locust.sh
+++ app/deploy-locust.sh
@@ -5,19 +5,17 @@ set -o nounset
 set -o xtrace
 # set -eox pipefail #safety for script
 
-git clone https://github.com/joakimhew/locust-kubernetes.git && cd locust-kubernetes && ls -lai
-
 # Set docker environment to minikube
-eval $(minikube docker-env)
+# eval $(minikube docker-env)
 
 # Initialize helm
 helm init
 
-# Build docker image
-docker build docker-image -t locust-tasks:latest
+# # Build docker image
+# docker build docker-image -t locust-tasks:latest
 
-# Install helm charts onto kubernetes cluster
-helm install loadtest-chart --name locust
+# # Install helm charts onto kubernetes cluster
+# helm install loadtest-chart --name locust
 
-# List services to find locust URL
-minikube service list
+# # List services to find locust URL
+# minikube service list
--- app/deploy-minikube_latest.sh
+++ app/deploy-minikube_latest.sh
@@ -30,6 +30,28 @@ minikube start --vm-driver=none \
                 --extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf \
                 --extra-config kubeadm.ignore-preflight-errors=SystemVerification
 
+# Set docker environment to minikube
+eval "$(minikube docker-env --profile=minikube)" && export DOCKER_CLI='docker'
+
+echo "=========================================================================================="
+kubectl version --client #ensure the version
+kubectl cluster-info
+minikube status
+echo "=========================================================================================="
+echo "Waiting for kubernetes to be ready ..."
+  for i in {1..150}; do # Timeout after 5 minutes, 150x2=300 secs
+    if kubectl get pods --namespace=kube-system -lk8s-app=kube-dns|grep Running ; then
+      break
+    fi
+    sleep 2
+  done
+echo "============================status check=============================================================="
+minikube status
+kubectl cluster-info
+kubectl get pods --all-namespaces;
+kubectl get pods -n default;
+kubectl get pod -o wide #The IP column will contain the internal cluster IP address for each pod.
+kubectl get service --all-namespaces # find a Service IP,list all services in all namespaces
 
 #
 # - sudo apt-get -qq -y install conntrack #X Sorry, Kubernetes v1.18.3 requires conntrack to be installed in root's path
