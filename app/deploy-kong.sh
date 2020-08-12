8b5d3c027dd981a0f08a5a618c78c504a615afc0
githubfoam <lequestionis@gmail.com>
2020-08-12 16:48:32 +0300

    reinit kong

--- app/deploy-kong.sh
+++ app/deploy-kong.sh
@@ -7,13 +7,42 @@ set -o xtrace
 
 # https://github.com/Kong/kong-dist-kubernetes
 # https://docs.konghq.com/1.4.x/kong-for-kubernetes/
+# https://github.com/Kong/kubernetes-ingress-controller/blob/main/docs/deployment/k4k8s.md
 echo "=============================deploy kong============================================================="
 
-kubectl apply -f https://raw.githubusercontent.com/Kong/kong-dist-kubernetes/master/minikube/postgres.yaml
-kubectl apply -f https://raw.githubusercontent.com/Kong/kong-dist-kubernetes/master/minikube/kong_migration_postgres.yaml
-kubectl apply -f https://raw.githubusercontent.com/Kong/kong-dist-kubernetes/master/minikube/kong_postgres.yam
+# kubectl apply -f https://raw.githubusercontent.com/Kong/kong-dist-kubernetes/master/minikube/postgres.yaml
+# kubectl apply -f https://raw.githubusercontent.com/Kong/kong-dist-kubernetes/master/minikube/kong_migration_postgres.yaml
+# kubectl apply -f https://raw.githubusercontent.com/Kong/kong-dist-kubernetes/master/minikube/kong_postgres.yam
 
-kubectl get deployment kong-rc
+# kubectl get deployment kong-rc
+
+# # Run the two mock services
+# docker build --no-cache -t kong:mesh-config  . -f Dockerfile.kong-config
+
+# kubectl apply -f serviceb.yaml
+# kubectl apply -f servicea.yaml
+
+# # Service B logs
+# kubectl logs -l app=serviceb -c serviceb
+
+# # Kong mesh logs
+# kubectl logs -l app=servicea -c kong
+
+
+# YAML manifests METHOD 1
+# deploy Kong via kubectl
+kubectl apply -f https://bit.ly/kong-ingress-dbless
+
+# Helm Chart METHOD 2
+# deploy Kong onto your Kubernetes cluster with Helm
+# helm repo add kong https://charts.konghq.com
+# helm repo update
+
+# Helm 2
+# helm install kong/kong
+
+# Helm 3
+# helm install kong/kong --generate-name --set ingressController.installCRDs=false
 
 
 # echo "=========================================================================================="
@@ -33,14 +62,4 @@ kubectl get pods -n default
 kubectl get pod -o wide #The IP column will contain the internal cluster IP address for each pod.
 kubectl get service --all-namespaces # find a Service IP,list all services in all namespaces
 
-# Run the two mock services
-docker build --no-cache -t kong:mesh-config  . -f Dockerfile.kong-config
 
-kubectl apply -f serviceb.yaml
-kubectl apply -f servicea.yaml
-
-# Service B logs
-kubectl logs -l app=serviceb -c serviceb
-
-# Kong mesh logs
-kubectl logs -l app=servicea -c kong
